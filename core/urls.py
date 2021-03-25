622f365b40b309e5c0372cc372aa76f95755d966
b3lzebuth <leandrofelipecunha@hotmail.com>
2021-03-25 17:41:18 -0300

    ajustes templates

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import IndexView, ErrorView
+from .views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('404/', ErrorView.as_view(), name='404')
 ]
--- core/views.py
+++ core/views.py
@@ -3,7 +3,3 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-
-
-class ErrorView(TemplateView):
-    template_name = '404.html'
