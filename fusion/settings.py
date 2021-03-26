a28730444e37b9663f6f4f7a19ae3ec674986fb1
b3lzebuth <leandrofelipecunha@hotmail.com>
2021-03-26 16:58:24 -0300

    models

--- fusion/settings.py
+++ fusion/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from secrets import DB_PASSWORD, SC_KEY
+from dev_secrets import DB_PASSWORD, SC_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SC_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
